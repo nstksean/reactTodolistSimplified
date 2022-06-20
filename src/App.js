9da6f3bd8428f271d14f447830839ce3a4af1ea3
linsean <linsean@LindeAir>
2022-06-21 03:01:59 +0800

    add counter and simple todoitem in app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import React from 'react';
+import Counter from './Counter';
+import TodoItem from './TodoItem';
 
 function App() {
+       
   return (
+    
     <div className="App">
-      <div className="card"> 
-            <div className="cardBody">
-                <h2>Simple Todo List</h2>
-                <div className="inputGroup mb-3">
-                    <input type="text" className="formCotrol input-todo" placeholder="What you want ToDo ?"/>
-                    <div className="inputGroupAppend">
-                        <button className="btn btn-add" type="button">新增</button>
-                    </div>
-                </div>
-                <ul className="listGroup">
-                    <li className="listGroupItem">
-                        todo1
-                        <div>
-                            <div className="btnGroup">
-                                <button type="button" className="btn unComplete">未完成</button>
-                                <button type="button" className="btn delete">刪除</button>
-                            </div>
-                        </div>
-                    </li>
-                    <li className="listGroupItem">
-                        todo2
-                        <div>
-                            <div className="btnGroup">
-                                <button type="button" className="btn unComplete">已完成</button>
-                                <button type="button" className="btn delete">刪除</button>
-                            </div>
-                        </div>
-                    </li>
-                    <li className="listGroupItem">
-                        todo3
-                        <div>
-                            <div className="btnGroup">
-                                <button type="button" className="btn unComplete">已完成</button>
-                                <button type="button" className="btn delete">刪除</button>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <div> <Counter/> </div>
+        <TodoItem content={123}/>
+        <TodoItem content={456}/>
     </div>
   );
 }
