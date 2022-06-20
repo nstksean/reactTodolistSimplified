39b4bf2bf405813f7eb5e8e7c57296cfff4129fd
linsean <linsean@LindeAir>
2022-06-20 11:13:48 +0800

    make reactApp transform to normal todo

--- src/App.js
+++ src/App.js
@@ -4,20 +4,46 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card"> 
+            <div className="cardBody">
+                <h2>Simple Todo List</h2>
+                <div className="inputGroup mb-3">
+                    <input type="text" className="formCotrol input-todo" placeholder="What you want ToDo ?"/>
+                    <div className="inputGroupAppend">
+                        <button className="btn btn-add" type="button">新增</button>
+                    </div>
+                </div>
+                <ul className="listGroup">
+                    <li className="listGroupItem">
+                        todo1
+                        <div>
+                            <div className="btnGroup">
+                                <button type="button" className="btn unComplete">未完成</button>
+                                <button type="button" className="btn delete">刪除</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="listGroupItem">
+                        todo2
+                        <div>
+                            <div className="btnGroup">
+                                <button type="button" className="btn unComplete">已完成</button>
+                                <button type="button" className="btn delete">刪除</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="listGroupItem">
+                        todo3
+                        <div>
+                            <div className="btnGroup">
+                                <button type="button" className="btn unComplete">已完成</button>
+                                <button type="button" className="btn delete">刪除</button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
   );
 }
