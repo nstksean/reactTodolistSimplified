8bf874e2764f1056266385815952dea7a1cf0b28
linsean <linsean@LindeAir>
2022-06-22 14:41:47 +0800

    Add todo cna add words

--- src/App.js
+++ src/App.js
@@ -3,15 +3,41 @@ import styled from 'styled-components';
 import React from 'react';
 import Counter from './Counter';
 import TodoItem from './TodoItem';
+import { useState,useRef } from 'react';
+
+
 
 function App() {
-       
+  
+  const [todos,setTodos] = useState([
+    {id:1, content:'abc'}
+  ])
+
+  const [value,setValue] = useState('')
+  const id = useRef (2)
+  
+  const handelButtonClick=()=>{
+    setTodos ([{
+      id:id.current,
+      content:value
+    },...todos])
+    setValue('')
+    id.current++
+  }
+
+  const handleInputChange = (e) =>{
+    setValue(e.target.value)
+  }
   return (
     
     <div className="App">
         <div> <Counter/> </div>
-        <TodoItem content={123}/>
-        <TodoItem content={456}/>
+        <input type="text" placeholder='todo' value={value} onChange={handleInputChange} />
+        <button onClick={handelButtonClick}>Add todo</button>
+        {
+        todos.map(todo => <TodoItem key={todo.id} todo={todo}/>)
+        }
+       
     </div>
   );
 }
