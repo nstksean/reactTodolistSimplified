37de55c46cab341b3a7ea966cb6a0cf9d91503bc
linsean <linsean@LindeAir>
2022-06-25 14:24:58 +0800

    feat: add form submit actions

--- src/App.js
+++ src/App.js
@@ -1,51 +1,46 @@
 import './App.css';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import React from 'react';
 import Counter from './Counter';
 import TodoItem from './TodoItem';
-import { useState,useRef } from 'react';
+import { useState, useRef } from 'react';
 
 
 
 function App() {
-  
-  const [todos,setTodos] = useState([
-    {id:1, content:'abc', isDone:true},
-    {id:2, content:"not", isDone:false}
+
+  const [todos, setTodos] = useState([
+    { id: 1, content: 'abc', isDone: true },
+    { id: 2, content: "not", isDone: false },
   ])
 
-  const [value,setValue] = useState('')
-  const id = useRef (3)
-  
-  const handleButtonClick=()=>{
-    setTodos ([{
-      id:id.current,
-      content:value,
-      isDone:false,
-    },...todos])
+  const [value, setValue] = useState('')
+  const id = useRef(3)
+
+  const handleButtonClick = (event) => {
+    console.log('event', event)
+    event.preventDefault()
+    setTodos([{
+      id: id.current,
+      content: value,
+      isDone: false,
+    }, ...todos])
     setValue('')
     id.current++
   }
 
-  const handleInputChange = (e) =>{
+  const handleInputChange = (e) => {
     setValue(e.target.value)
   }
 
-  const handleDeleteTodo = id =>{
+  const handleDeleteTodo = id => {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
-  const handleKeyDowm = (e)=>{
-
-  
-  if (e.key === 'Enter' ){
-    handleButtonClick()
-  }}
-  
-  const handleToggleIsDone = id =>{
-    setTodos(todos.map(todo=>{
-      if(todo.id !== id )return todo
-      return{
+  const handleToggleIsDone = id => {
+    setTodos(todos.map(todo => {
+      if (todo.id !== id) return todo
+      return {
         ...todo,
         isDone: !todo.isDone
       }
@@ -53,24 +48,27 @@ function App() {
   }
 
   return (
-    
     <div className="App">
-        <div> <Counter/> </div>
-        <input 
-        type="text" 
-        placeholder='todo' 
-        value={value} 
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDowm}
-          />
+      <div> <Counter /> </div>
+
+      <form onSubmit={handleButtonClick}>
+        <input
+          type="text"
+          name='data'
+          placeholder='todo'
+          value={value}
+          onChange={handleInputChange}
+        />
         <button onClick={handleButtonClick}>Add todo</button>
-        {
+      </form>
+
+      {
         todos.map(todo => <TodoItem key={todo.id} todo={todo}
-        handleDeleteTodo={handleDeleteTodo} handleToggleIsDone={handleToggleIsDone} />)
-        }
-       
+          handleDeleteTodo={handleDeleteTodo} handleToggleIsDone={handleToggleIsDone} />)
+      }
+
     </div>
   );
-      }
+}
 
 export default App;
